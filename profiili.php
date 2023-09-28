798b2cf9fa304ccf9ffdcd5bbe64f5f46b4f47cd
nawzad-hassan <143536827+nawzad-hassan@users.noreply.github.com>
2023-09-28 19:52:39 +0300

    Profile code

---
+++ profiili.php
@@ -0,0 +1,36 @@
+<?php
+
+//$loggedIn = secure_page();
+$title = 'Profiili';
+$css = 'profiili.css';
+include 'header.php';
+
+?>
+
+<div class="container">
+<img src="IMG_0100.jpg" alt="Profile Picture">
+<div class="info-section">
+    <div class="info-title">Nimi:</div>
+    <div>Nawzad Hassan</div>
+</div>
+
+<div class="info-section">
+    <div class="info-title">Koulutus:</div>
+    <div>Diplomi-insinööri</div>
+</div>
+<br>
+<div class="info-section">
+    <div class="info-title">Yhteystiedot:</div>
+    <div>Email:nawzad.yousif@hotmail.com</div>
+    <div>Puh. 0451378233</div>
+    <div>Osoite: Ilotulitusyie 9 B 25, 00930 Helsinki</div>
+</div>
+<br>
+<div class="harrastukset">
+    <div class="info-title">Harrastukset:</div>
+    <div>Jalkapallo</div>
+    <div>Musiikki</div>
+    <div>Tietokone</div>
+    <div>Uinti</div>
+</div>
+
